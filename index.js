a149f4dc860c55600f494586b07aa14b6af0831f
valeraqwe <valera.komar.1999@gmail.com>
2023-05-31 17:40:27 +0300

    creating user schema and dotenv

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const app = require('./app')
-
-app.listen(4000, () => console.log('Server is running at port 4000...'))
+const {PORT} = process.env
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}...`))
